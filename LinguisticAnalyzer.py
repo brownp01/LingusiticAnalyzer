c8b973964f02a13e7ce8de6f4eff3fd2fc39d5d4
tlblanton <tylerblanton635@gmail.com>
2017-09-08 20:05:29 -0600

    Added comments for clarity

--- LinguisticAnalyzer.py
+++ LinguisticAnalyzer.py
@@ -4,20 +4,24 @@ from flask import Response, request, redirect
 app = Flask(__name__)
 
 
+# Default route, with no additions to the URI
 @app.route('/')
 def hello_world():
-    jsonString = '{"Project" : "Linguistic Analyzer", "Author" : "Tyler Blanton and Paul Brown", "Client" : "Benjamin Pope"  }'
+    jsonString = '{"Project":"Linguistic Analyzer", "Author":"Tyler Blanton and Paul Brown", "Client":"Benjamin Pope"}'
     return Response(jsonString, content_type='application/json')
 
 
-@app.route('/Test', methods=['GET', 'POST'])
+# Route with /Test ('http://127.0.0.1:5000/Test') accepts both posts and puts
+@app.route('/Test', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def test():
     if request.method == 'POST':
+        # redirecting the user to the same page in the case of a POST
         return redirect(request.url)
 
     elif request.method == 'GET':
+        # returning hard-coded html in the case of a GET
         htmlInputString = """
-            <form action="http://127.0.0.1:5000/TylerTest"
+            <form action="http://127.0.0.1:5000/Test"
             enctype="multipart/form-data" method="post">
             <p>
             Type some text (if you like):<br>
@@ -32,10 +36,12 @@ def test():
             </div>
             </form>
             """
-        return Response(htmlInputString)
+        # mimetype is the response type
+        return Response(htmlInputString, mimetype='text/html')
     else:
+        # in other cases, we return what type of request it was
         retJson = '{"Request.method" : "' + request.method + '"}'
-        return Response(retJson)
+        return Response(retJson, mimetype='application/json')
 
 
 if __name__ == '__main__':
