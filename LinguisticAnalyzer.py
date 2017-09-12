444c5cb7de8541b71b891adc2227cb0c94de87e1
tlblanton <tylerblanton635@gmail.com>
2017-09-12 17:37:18 -0600

    Added simple methods to call to outside API and to parse incoming json data from a POST request.

--- LinguisticAnalyzer.py
+++ LinguisticAnalyzer.py
@@ -1,47 +1,50 @@
 from flask import Flask
 from flask import Response, request, redirect
+import simplejson
+import requests
+import json
+from pdfminer import converter
+from v1 import analyze
 
 app = Flask(__name__)
 
 
-# Default route, with no additions to the URI
 @app.route('/')
+def default():
+    f = open("constants/index.html", "r")  # opens file with name of "index.html"
+    return Response(f.read(), mimetype='text/html')
+
+
+# Project route, with no additions to the URI
+@app.route('/project')
 def hello_world():
     jsonString = '{"Project":"Linguistic Analyzer", "Author":"Tyler Blanton and Paul Brown", "Client":"Benjamin Pope"}'
     return Response(jsonString, content_type='application/json')
 
 
 # Route with /Test ('http://127.0.0.1:5000/Test') accepts both posts and puts
+
 @app.route('/Test', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def test():
     if request.method == 'POST':
-        # redirecting the user to the same page in the case of a POST
-        return redirect(request.url)
+        # This POST parses json data
+        jsonData = request.get_json()
+        print(jsonData["this"])
+        return Response('<p>POST response. </p>', mimetype='text/html')
 
     elif request.method == 'GET':
-        # returning hard-coded html in the case of a GET
-        htmlInputString = """
-            <form action="http://127.0.0.1:5000/Test"
-            enctype="multipart/form-data" method="post">
-            <p>
-            Type some text (if you like):<br>
-            <input type="text" name="textline" size="30">
-            </p>
-            <p>
-            Please specify a file, or a set of files:<br>
-            <input type="file" name="datafile" size="40">
-            </p>
-            <div>
-            <input type="submit" value="Send">
-            </div>
-            </form>
-            """
-        # mimetype is the response type
-        return Response(htmlInputString, mimetype='text/html')
+        # This GET samples calling to an API
+        r = requests.get('https://api.github.com/users/tlblanton')
+        return Response(r.text, mimetype='application/json')
     else:
         # in other cases, we return what type of request it was
         retJson = '{"Request.method" : "' + request.method + '"}'
         return Response(retJson, mimetype='application/json')
+
+
+@app.route('/api/v1/analyze', methods=['POST'])
+def analyze():
+    return Response('<p>api/v1/analyze placeholder</p>', mimetype='text/html')
 
 
 if __name__ == '__main__':
---
+++ v1/analyze.py
@@ -0,0 +1,6 @@
+from flask import Response
+
+
+# def analyze(request):
+#     print(request.data)
+#     return Response("{'data' : '" + str(request.data) + '}"', mimetype='application/json')
