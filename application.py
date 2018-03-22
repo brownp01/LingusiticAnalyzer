9d6584139a38442f074f934bd3fcda8b7e6f26e5
Paul <paul.brown@ucdenver.edu>
2018-03-22 13:51:19 -0600

    updated comments in application.py for documentation.

--- application.py
+++ application.py
@@ -22,12 +22,14 @@ def resource_path(relative_path):
     """
     Summary: Function to determine correct file path of directories for use within an IDE or executable.
 
+
     :param str relative_path: the path of a directory relative to a local environment
-    :return: base_path in relation to executable environment and relative_path of local environment
-    :rtype: string
+    :return: *base_path* in relation to executable environment and *relative_path* of local environment
+    :rtype: str
 
     """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+
     return os.path.join(base_path, relative_path)
 
 
@@ -38,112 +40,139 @@ VIEWS = resource_path("views/")
 @application.route('/')
 def main():
     """
-    Home page of the Linguistic Analyzer API
+    Summary: Home page of the Linguistic Analyzer API.
+
+    A *loggerStart* variable is used here to keep track of home page instances in relation to logging.
+    :func:`analyze_functions.declarelogger` is used to start a new instance of the log.
 
     :return: Home page
     :rtype: html
+
     """
 
     #flash('temp', category='message')
     #get_flashed_messages()
 
-    common_functions.writeToConfig('NEW_DOC_FLAG', 'false')
+    #sets the NEW_DOC_FLAG to false as a default setting. NEW_DOC_FLAG indicates whether a regulatory document was dynamically
+    #added to the app.
 
+    common_functions.writeToConfig('NEW_DOC_FLAG', 'false')
 
     global loggerStart
 
     # Creating new log file every time the program starts.
     if loggerStart == 0:
         analyze_functions.declarelogger()
-    if loggerStart > 3:
+    if loggerStart > 10:
         analyze_functions.declarelogger()
         loggerStart = 0
 
     f = open(VIEWS + "index.html", "r")  # opens file with name of "index.html"
     loggerStart += 1
+
     return Response(f.read(), mimetype='text/html')
 
 
 @application.route('/bubbletest')
 def bubbletest():
     """
-        Page for testing
+    Summary: Page for bubble chart testing.
 
-        :return: Test page
-        :rtype: html
-        """
+
+    :return: Test page
+    :rtype: html
+
+     """
+
     # Creating new log file every time the program starts.
     if common_functions.homeCount() == 0:
         analyze_functions.declarelogger()
+
     f = open(VIEWS + "test.html", "r")  # opens file with name of "index.html"
+
     return Response(f.read(), mimetype='text/html')
 
 
 @application.route('/reusablebubble')
 def reusablebubble():
     """
-            Page for testing
+    Summary: Page for bubble chart testing.
 
-            :return: Test page
-            :rtype: html
-            """
+
+    :return: Test page
+    :rtype: html
+
+     """
 
     f = open(VIEWS + "reusable_bubble.html", "r")  # opens file with name of "reusable_bubble.html"
+
     return Response(f.read(), mimetype='text/html')
 
 
 @application.route('/keywordbubblechart')
 def keywordbubblechart():
     """
-        Returns bubble chart html page
+    Summary: Returns bubble chart html page.
 
-        :return: bubble chart html page
-        :rtype: html
+    :return: bubble chart html page
+    :rtype: html
 
     """
+
     f = open(VIEWS + "bubble_chart.html", "r")  # opens file with name of "reusable_bubble.html"
+
     return Response(f.read(), mimetype='text/html')
 
 
 @application.route('/reusablebubble.js')
 def reusablebubblejs():
     """
-            Page for testing
+    Summary: Page for bubble chart testing.
 
-            :return: Test page
-            :rtype: html
-            """
+    :return: Test page
+    :rtype: html
+
+    """
+
     f = open(VIEWS + "js/reusable_bubble.js", "r")  # opens file with name of "index.html"
+
     return Response(f.read(), mimetype='text/html')
 
 
 @application.route('/index.js')
 def indexjs():
     """
-            Page for testing
+    Summary: Page for testing
 
-            :return: Test page
-            :rtype: html
-            """
+    :return: Test page
+    :rtype: html
+
+    """
+
     # Creating new log file every time the program starts.
     if common_functions.homeCount() == 0:
         analyze_functions.declarelogger()
+
     f = open(VIEWS + "js/index.js", "r")  # opens file with name of "index.html"
+
     return Response(f.read(), mimetype='text/javascript')
 
 
 @application.route('/project')
 def project():
     """
-    Returns an html page containing details about the Linguistic Analyzer project.
+    Summary: Returns an html page containing details about the Linguistic Analyzer project.
 
-    :return: Home page
+
+    :return: App summary
     :rtype: html
+
     """
 
     common_functions.writeToConfig('NEW_DOC_FLAG', 'false')
 
     f = open(VIEWS + "info.html", "r")  # opens file with name of "index.html"
+
     return Response(f.read().replace('#--DESCRIPTION_TITLE--#', 'Project Information').replace('#--DESCRIPTION--#',
                     "This Linguistic Analyzer lets the user upload a description document and compares that document \
                     against a regulatory document using Yule's k and Yule's i Algorithms, as well as a keyword scores algorithm.\
@@ -153,10 +182,34 @@ def project():
 @application.route('/analyze', methods=['POST'])
 def analyze():
     """
-    Receives uploaded document and comparison document choice and executes logic to compare them.
+    Summary: Receives uploaded document and comparison document choice and executes various functions to compare them.
+
+    - This functions starts by verifying that a valid file has been uploaded. Error handling also occurs here if no
+      file has been selected.
+    - Process User Document
+
+        - All functions relating to the analysis of a user doc is handled by :func:`common_functions.interpretfile`.
+
+    - Process Regulatory Document
+
+        - All functions relating to the analysis of a reg doc is handled by :func:`common_functions.interpretexistingfile`.
+
+    - Process Various Graph Displays
+
+        - All functions related to processing graphical displays of the analyzed data is handled here.
+
+            - :func:`common_functions.plotkeywordsalience`.
+            - :func:`common_functions.plotkeywordscores`.
+            - :func:`common_functions.plotkeywordfrequency`.
+            - :func:`common_functions.generatebubblecsv`.
+
+    An **Exception** will be raised if there is an error in the analysis process. An *"Unknown Error Has Occurred"* web
+    page will be displayed.
 
     :return: Information regarding the uploaded document's similarity to regulatory document
     :rtype: html
+    :raises: Exception
+
     """
 
     # resetting document flag in case it was previously set when adding a new regulatory document to the list.
@@ -233,11 +286,13 @@ def analyze():
 @application.route('/backgroundimg', methods=['GET'])
 def getbackgroundimg():
     """
-        Returns png image of file at
+    Summary: Returns *png* image of file.
 
-        :return: graph
-        :rtype: png
-        """
+    :return: graph
+    :rtype: png
+
+    """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='jpg')
     pilImage = open('views/img/background.png', 'rb')
     copyfileobj(pilImage, tempFileObj)
@@ -250,15 +305,14 @@ def getbackgroundimg():
 @application.route('/keywordsalienceimage', methods=['GET'])
 def getkwsalienceimage():
     """
-    Returns png image of a graph of top salience keywords
+    Summary: Returns *png* image of a graph of top salience keywords.
 
     :return: graph
     :rtype: png
+
     """
 
     return_html = ''
-
-
 
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='jpg')
     pilImage = open('downloads/topsalience.png', 'rb')
@@ -272,15 +326,14 @@ def getkwsalienceimage():
 @application.route('/backgroundwordsimg', methods=['GET'])
 def getbackgroundwordsimg():
     """
-    Returns png image of a graph of words background
+    Summary: Returns *png* image of a graph of words background.
 
     :return: graph
     :rtype: png
+
     """
 
     return_html = ''
-
-
 
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='jpg')
     pilImage = open('views/img/words_background_1.jpg', 'rb')
@@ -294,11 +347,13 @@ def getbackgroundwordsimg():
 @application.route('/keywordscoresimage', methods=['GET'])
 def getkwscoresimage():
     """
-    Returns png image of a graph of keyword scores
+    Summary: Returns *png* image of a graph of keyword scores.
 
     :return: graph
     :rtype: png
+
     """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='jpg')
     pilImage = open('downloads/topkeywordscores.png', 'rb')
     copyfileobj(pilImage, tempFileObj)
@@ -311,11 +366,13 @@ def getkwscoresimage():
 @application.route('/keywordfrequencyimage', methods=['GET'])
 def getkwfreeqimage():
     """
-    Returns Keyword frequency graph
+    Summary: Returns *png* image of a keyword frequency graph.
 
     :return: graph
     :rtype: png
+
     """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='jpg')
     pilImage = open('downloads/topkeywordfrequency.png', 'rb')
     copyfileobj(pilImage, tempFileObj)
@@ -327,91 +384,110 @@ def getkwfreeqimage():
 @application.route('/linguistic_analyzer_log', methods=['GET'])
 def getlinguisticanalyzerlog():
     """
-        Returns LinguisticAnalyzer.log
+    Summary: Returns "LinguisticAnalyzer.log".
 
-        :return: log file
-        :rtype: .log
+    :return: log file
+    :rtype: log
+
     """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='log')
     pilImage = open('logging/Linguistic_Analyzer.log', 'rb')
     copyfileobj(pilImage, tempFileObj)
     pilImage.close()
     tempFileObj.seek(0, 0)
+
     return send_file(tempFileObj, as_attachment=False, attachment_filename='Linguistic_Analyzer.log')
 
 
 @application.route('/user_doc_kws', methods=['GET'])
 def getuserdockws():
     """
-        Returns Keywords.txt
+    Summary: Returns "Keywords.txt" which contains information regarding a user's document keywords.
 
-        :return: keyword file
-        :rtype: .txt
+    :return: keyword file
+    :rtype: txt
+
     """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='log')
     pilImage = open('Documents/Keywords.txt', 'rb')
     copyfileobj(pilImage, tempFileObj)
     pilImage.close()
     tempFileObj.seek(0, 0)
+
     return send_file(tempFileObj, as_attachment=False, attachment_filename='UserDocKeywords.txt')
 
 
 @application.route('/reg_doc_kws', methods=['GET'])
 def getregdockws():
     """
-        Returns Reg_Keywords.txt
+    Summary: Returns "Reg_Keywords.txt" which contains information regarding a regulatory document's keywords.
 
-        :return: regulatory doc keyword file
-        :rtype: .txt
+    :return: regulatory doc keyword file
+    :rtype: txt
+
     """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='log')
     pilImage = open('Documents/Reg_Keywords.txt', 'rb')
     copyfileobj(pilImage, tempFileObj)
     pilImage.close()
     tempFileObj.seek(0, 0)
+
     return send_file(tempFileObj, as_attachment=False, attachment_filename='RegDocKeywords.txt')
 
 
 @application.route('/testkeywords', methods=['GET'])
 def gettestkeywords():
     """
-        Returns test_keywords.csv
+    Summary: Returns "test_keywords.csv" which contains keywords for bubble chart information.
 
-        :return: test_keywords doc keyword file
-        :rtype: .csv
+    :return: test_keywords doc keyword file
+    :rtype: csv
+
     """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='log')
     pilImage = open('Documents/test_keywords.csv', 'rb')
     copyfileobj(pilImage, tempFileObj)
     pilImage.close()
     tempFileObj.seek(0, 0)
+
     return send_file(tempFileObj, as_attachment=False, attachment_filename='test_keywords.csv')
+
 
 @application.route('/csvkeywords', methods=['GET'])
 def getcsvkeywords():
     """
-        Returns csvkeywords.csv
+    Summary: Returns "csvkeywords.csv" which contains keywords for bubble chart information.
 
-        :return: csvkeywords keyword file
-        :rtype: .csv
+    :return: csvkeywords keyword file
+    :rtype: csv
+
     """
+
     tempFileObj = NamedTemporaryFile(mode='w+b', suffix='log')
     pilImage = open('Documents/csvkeywords.csv', 'rb')
     copyfileobj(pilImage, tempFileObj)
     pilImage.close()
     tempFileObj.seek(0, 0)
+
     return send_file(tempFileObj, as_attachment=False, attachment_filename='csvkeywords.csv')
 
 
 @application.route('/yulesinfo', methods=['GET'])
 def yulesinfo():
     """
-    Yule's Info
+    Summary: Returns an html page regarding Yule's Info.
 
-    :return: Page that describes Yule's k and Yule's i algorithms
+    :return: Page that describes Yule's K and Yule's I algorithms
     :rtype: html
+
     """
+
     f = open("views/info.html", "r")  # opens file with name of "index.html"
+
     return Response(f.read().replace('#--DESCRIPTION_TITLE--#', "Yule's k and Yule's i algorithms")\
         .replace('#--DESCRIPTION--#', "\"Yule's k\" and \"Yule's i\" are calculated values that represent the \
         semantic richness of a given text. We utilize this algorithm because semantic richness is one benchmark by \
@@ -423,12 +499,15 @@ def yulesinfo():
 @application.route('/comparisoninfo', methods=['GET'])
 def comparisoninfo():
     """
-    Comparison Information
+    Summary: Returns an html page regarding comparison score information.
 
     :return: graph html page that describes the Linguistic Analyzer's Comparison Score
     :rtype: html
+
     """
+
     f = open("views/info.html", "r")  # opens file with name of "index.html"
+
     return Response(f.read().replace('#--DESCRIPTION_TITLE--#', "Yule's k and Yule's i algorithms") \
                     .replace('#--DESCRIPTION--#', "This score takes into account the calculated score, \
                     frequency, and adjacent words for each keyword in both documents. If the given documents are \
@@ -440,10 +519,10 @@ def comparisoninfo():
 @application.route('/applicationconfig', methods=['GET'])
 def getapplicationconfig():
     """
-        Returns json application config file
+    Summary: Returns a json application config file.
 
-     :return: applicationconfig.json
-     :rtype: json file
+    :return: */applicationconfig.json*
+    :rtype: json
 
     """
 
@@ -457,10 +536,15 @@ def getapplicationconfig():
 @application.route('/newregdoc', methods=['POST'])
 def newregdoc():
     """
-    Adds new regulatory document
+    Summary: Dynamically adds a new regulatory document to the app.
 
-    :return: none
-    :rtype: none
+    - A new regulatory document is saved into the app via :func:`common_functions.savefile`.
+    - */views/index.html* is edited to include a reference to the new regulatory document so it can be selected by user.
+    - Home page is reloaded with newly added document reference. 
+
+    :return: updated */views/index.html* page
+    :rtype: html
+
     """
 
     common_functions.writeToConfig('NEW_DOC_FLAG', 'true')
@@ -496,9 +580,10 @@ def newregdoc():
 @application.route('/documentationredirect', methods=['GET'])
 def getdocumentationhome():
     """
-        Returns index page nested in Documentation/_build/html which is the home page for our Sphinx-generated documentation
+    Summary: Returns redirect page where app documentation is referenced.
 
-        :return: html text
+    :return: html text
+    :rtype: str
 
     """
     # f = open('file:///', 'r')
@@ -508,9 +593,6 @@ def getdocumentationhome():
 
     #return Response(html, mimetype='text/html')
     return redirect('https://tlblanton.github.io/LinguisticAnalyzer/', code=302)
-
-
-
 
 
 
